1127932c6807b3e8f93db7f6a52ea0bfbbf28f1c
Kyle Rusby <8283338+RusbyProgramming@users.noreply.github.com>
2017-10-26 19:28:38 +0100

    Initial commit

---
+++ PongClient/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PongClient
+{
+    public partial class Form1 : Form
+    {
+        public Bitmap buffer;
+        private Timer gameTimer;
+        private Game game;
+        private bool firstRun;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            //Set firstRun
+            this.firstRun = false;
+
+            //Set the winform style ready for painting
+            this.SetStyle(
+            ControlStyles.UserPaint |
+            ControlStyles.AllPaintingInWmPaint |
+            ControlStyles.DoubleBuffer, true);
+
+            //Start gametick
+            this.gameTimer = new Timer() { Interval = 10 };
+            this.gameTimer.Tick += new EventHandler(gameTick);
+            this.gameTimer.Start();
+
+            //Events for painting to the form
+            this.ResizeEnd += new EventHandler(createBuffer);
+            this.Load += new EventHandler(createBuffer);
+            this.Paint += new PaintEventHandler(paint);
+            this.KeyDown += Form1_KeyDown;
+        }
+
+        private void gameTick(object sender, EventArgs e)
+        {
+            if (this.buffer != null)
+            {
+                //Creates game object with a reference to the forms buffer
+                if (!this.firstRun)
+                {
+                    this.game = new Game(ref this.buffer);
+                    this.firstRun = true;
+                }
+
+                //Game controller with states
+                switch (this.game.CurrentState)
+                {
+                    case Game.State.Connecting:
+                        this.game.Connect(this.ClientSize.Width, this.ClientSize.Height);
+                        break;
+                }
+
+
+                //Invalidates the controls on each tick
+                this.Invalidate();
+            }
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            //throw new NotImplementedException();
+        }
+
+        private void createBuffer(object sender, EventArgs e)
+        {
+            if (this.buffer != null) //Disposes of buffer at the start
+                this.buffer.Dispose();
+
+            //Initalizes buffer
+            this.buffer = new Bitmap(this.ClientSize.Width, this.ClientSize.Height);
+        }
+
+ 
+        private void paint(object sender, PaintEventArgs e)
+        {
+            if (this.buffer != null)
+            {
+                //Draws buffer to the form
+                e.Graphics.DrawImageUnscaled(this.buffer, Point.Empty);
+            }
+        }
+    }
+}
---
+++ PongClient/Game.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace PongClient
+{
+    class Game
+    {
+        //State of game
+        public State CurrentState { get; private set; }
+
+        private TcpClient client;
+        private byte[] clientBuffer;
+        private Graphics frmGraphics;
+
+        public Game(ref Bitmap g)
+        {
+            //Set state to first state
+            this.CurrentState = State.Connecting;
+
+            //Set frmGraphics to the forms bitmap object which allows for writing to the screen
+            this.frmGraphics = Graphics.FromImage(g);
+        }
+
+        public void Connect(int x, int y)
+        {
+            //Draw connecting to form
+            this.frmGraphics.DrawString("Connecting...", new Font(FontFamily.GenericSansSerif, 40, FontStyle.Regular), Brushes.White, (x / 4), (y / 3));
+
+            //Begin connecting to server
+            //this.client = new TcpClient();
+            //this.client.ConnectAsync("127.0.0.1", 8122).Wait();
+
+            //Start connection thread
+            //new Thread(this.beginRead).Start();
+        }
+
+        private void beginRead()
+        {
+            try
+            {
+                //Set the size of client buffer to 6 bytes and start read. 2 = Packet ID, 4 = Size of packet
+                this.clientBuffer = new byte[6];
+                this.client.GetStream().BeginRead(this.clientBuffer, 0, this.clientBuffer.Length, this.readPacket, null);
+            }
+            catch(Exception ex)
+            {
+                
+            }
+        }
+
+        private void readPacket(IAsyncResult ar)
+        {
+            //Check if length is correct
+            int read = this.client.GetStream().EndRead(ar);
+            if (read < this.clientBuffer.Length)
+                throw new Exception();
+
+            short packetID = 0;
+            int packetSize = 0;
+
+            //Read memory from buffer stream
+            using (BinaryReader br = new BinaryReader(new MemoryStream(this.clientBuffer)))
+            {
+                packetID = br.ReadInt16();
+                packetSize = br.ReadInt32();
+            }
+
+            //Get prepared for packet and read
+            this.clientBuffer = new byte[packetSize];
+            this.client.GetStream().ReadAsync(this.clientBuffer, 0, packetSize).Wait();
+
+
+        }
+
+        //Game states
+        public enum State
+        {
+            Connecting,
+            WaitingOtherPlayer,
+            Started,
+            Ended,
+        }
+    }
+}
